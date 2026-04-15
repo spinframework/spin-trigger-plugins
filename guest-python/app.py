b0471556567c845b3c144cc5fcb7a7aff0392261
Joel Dice <joel.dice@akamai.com>
2026-04-15 11:31:55 -0600

    fix Python example
    Signed-off-by: itowlson <ivan.towlson@fermyon.com>

--- guest-python/app.py
+++ guest-python/app.py
@@ -1,8 +1,8 @@
-from bindings import wit_world
-from bindings import cron_types
+import spin_cron
+from spin_cron.imports.spin_cron_cron_types_3_0_0 import Metadata
 from spin_sdk import variables
 
-class SpinCron(wit_world.WitWorld):
-    async def handle_cron_event(self, metadata: cron_types.Metadata) -> None:
+class SpinCron(spin_cron.SpinCron):
+    async def handle_cron_event(self, metadata: Metadata) -> None:
         temp = await variables.get("something")
         print("[" + str(metadata.timestamp) +"] " + "Hello every " + temp)
